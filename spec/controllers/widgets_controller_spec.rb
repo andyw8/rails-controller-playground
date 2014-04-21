b3ed38711d986c9f7d7cd73429be4a5cd7ceaa75
Andy Waite <github.aw@andywaite.com>
2014-04-21 14:50:38 +0200

    Let's rename widget_double to widget

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe WidgetsController do
   let(:valid_attributes) { { "name" => "MyString" } }
   let(:valid_session) { {} }
-  let(:widget_double) { double }
+  let(:widget) { mock_model('Widget') }
 
   describe "GET index" do
     it "assigns all widgets as @widgets" do
@@ -16,50 +16,50 @@ describe WidgetsController do
 
   describe "GET show" do
     it "assigns the requested widget as @widget" do
-      expect(Widget).to receive(:find).with("7").and_return(widget_double)
+      expect(Widget).to receive(:find).with("7").and_return(widget)
       get :show, {:id => 7}, valid_session
-      expect(assigns(:widget)).to eq(widget_double)
+      expect(assigns(:widget)).to eq(widget)
     end
   end
 
   describe "GET new" do
     it "assigns a new widget as @widget" do
-      expect(Widget).to receive(:new).and_return(widget_double)
+      expect(Widget).to receive(:new).and_return(widget)
       get :new, {}, valid_session
-      expect(assigns(:widget)).to eq(widget_double)
+      expect(assigns(:widget)).to eq(widget)
     end
   end
 
   describe "GET edit" do
     it "assigns the requested widget as @widget" do
-      expect(Widget).to receive(:find).with("7").and_return(widget_double)
+      expect(Widget).to receive(:find).with("7").and_return(widget)
       get :edit, {:id => 7}, valid_session
-      expect(assigns(:widget)).to eq(widget_double)
+      expect(assigns(:widget)).to eq(widget)
     end
   end
 
   describe "POST create" do
     describe "with valid params" do
-      let(:widget_double) { mock_model('Widget') }
+      let(:widget) { mock_model('Widget') }
       before do
-        expect(Widget).to receive(:new).with(valid_attributes).and_return(widget_double)
+        expect(Widget).to receive(:new).with(valid_attributes).and_return(widget)
       end
 
       it "creates a new Widget" do
-        expect(widget_double).to receive(:save)
+        expect(widget).to receive(:save)
         post :create, {:widget => valid_attributes}, valid_session
       end
 
       it "assigns a newly created widget as @widget" do
-        expect(widget_double).to receive(:save)
+        expect(widget).to receive(:save)
         post :create, {:widget => valid_attributes}, valid_session
-        expect(assigns(:widget)).to eq(widget_double)
+        expect(assigns(:widget)).to eq(widget)
       end
 
       it "redirects to the created widget" do
-        expect(widget_double).to receive(:save).and_return(true)
+        expect(widget).to receive(:save).and_return(true)
         post :create, {:widget => valid_attributes}, valid_session
-        expect(response).to redirect_to(widget_double)
+        expect(response).to redirect_to(widget)
       end
     end
 
@@ -82,37 +82,37 @@ describe WidgetsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested widget" do
-        expect(Widget).to receive(:find).with("7").and_return(widget_double)
-        expect(widget_double).to receive(:update).with({ "name" => "MyString" })
+        expect(Widget).to receive(:find).with("7").and_return(widget)
+        expect(widget).to receive(:update).with({ "name" => "MyString" })
         put :update, {:id => 7, :widget => { "name" => "MyString" }}, valid_session
       end
 
       it "assigns the requested widget as @widget" do
-        allow(Widget).to receive(:find).and_return(widget_double)
-        allow(widget_double).to receive(:update).with({ "name" => "MyString" })
+        allow(Widget).to receive(:find).and_return(widget)
+        allow(widget).to receive(:update).with({ "name" => "MyString" })
         put :update, {:id => 7, :widget => valid_attributes}, valid_session
-        expect(assigns(:widget)).to eq(widget_double)
+        expect(assigns(:widget)).to eq(widget)
       end
 
       it "redirects to the widget" do
-        widget_double = mock_model('Widget')
-        allow(Widget).to receive(:find).and_return(widget_double)
-        allow(widget_double).to receive(:update).with({ "name" => "MyString" }).and_return(true)
+        widget = mock_model('Widget')
+        allow(Widget).to receive(:find).and_return(widget)
+        allow(widget).to receive(:update).with({ "name" => "MyString" }).and_return(true)
         put :update, {:id => 7, :widget => valid_attributes}, valid_session
-        expect(response).to redirect_to(widget_double)
+        expect(response).to redirect_to(widget)
       end
     end
 
     describe "with invalid params" do
-      let(:widget_double) { mock_model('Widget') }
+      let(:widget) { mock_model('Widget') }
       before do
-        expect(widget_double).to receive(:update).and_return(false)
-        expect(Widget).to receive(:find).and_return(widget_double)
+        expect(widget).to receive(:update).and_return(false)
+        expect(Widget).to receive(:find).and_return(widget)
         put :update, {:id => 7, :widget => { "name" => "invalid value" }}, valid_session
       end
 
       it "assigns the widget as @widget" do
-        expect(assigns(:widget)).to eq(widget_double)
+        expect(assigns(:widget)).to eq(widget)
       end
 
       it "re-renders the 'edit' template" do
@@ -122,19 +122,19 @@ describe WidgetsController do
   end
 
   describe "DELETE destroy" do
-    let(:widget_double) { mock_model('Widget') }
+    let(:widget) { mock_model('Widget') }
 
     before do
-      expect(Widget).to receive(:find).and_return(widget_double)
+      expect(Widget).to receive(:find).and_return(widget)
     end
 
     it "destroys the requested widget" do
-      expect(widget_double).to receive(:destroy)
-      delete :destroy, {:id => widget_double.to_param}, valid_session
+      expect(widget).to receive(:destroy)
+      delete :destroy, {:id => widget.to_param}, valid_session
     end
 
     it "redirects to the widgets list" do
-      delete :destroy, {:id => widget_double.to_param}, valid_session
+      delete :destroy, {:id => widget.to_param}, valid_session
       expect(response).to redirect_to(widgets_url)
     end
   end
