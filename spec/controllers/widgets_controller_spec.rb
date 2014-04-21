5659814702f2b26c62f9c7a045e90f4fe67e83fb
Andy Waite <github.aw@andywaite.com>
2014-04-21 16:29:46 +0200

    Make sure the action happens in the spec, rather than the before block.
    This makes the specs longer but I think it's clearer.

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -4,6 +4,7 @@ describe WidgetsController do
   let(:valid_attributes) { { "name" => "MyString" } }
   let(:valid_session) { {} }
   let(:widget) { mock_model('Widget') }
+  before { allow(Widget).to receive(:find).with(widget.to_param).and_return(widget) }
 
   describe "GET index" do
     it "assigns all widgets as @widgets" do
@@ -16,7 +17,6 @@ describe WidgetsController do
 
   describe "GET show" do
     it "assigns the requested widget as @widget" do
-      allow(Widget).to receive(:find).with(widget.to_param).and_return(widget)
       get :show, {:id => widget.to_param}, valid_session
       expect(assigns(:widget)).to eq(widget)
     end
@@ -32,7 +32,6 @@ describe WidgetsController do
 
   describe "GET edit" do
     it "assigns the requested widget as @widget" do
-      allow(Widget).to receive(:find).with(widget.to_param).and_return(widget)
       get :edit, {:id => widget.to_param}, valid_session
       expect(assigns(:widget)).to eq(widget)
     end
@@ -61,16 +60,15 @@ describe WidgetsController do
     end
 
     describe "with invalid params" do
-      before do
-        allow_any_instance_of(Widget).to receive(:save).and_return(false)
-        post :create, {:widget => { "name" => "invalid value" }}, valid_session
-      end
+      before { allow_any_instance_of(Widget).to receive(:save).and_return(false) }
 
       it "assigns a newly created but unsaved widget as @widget" do
+        post :create, {:widget => { "name" => "invalid value" }}, valid_session
         expect(assigns(:widget)).to be_a_new(Widget) # fix this later
       end
 
       it "re-renders the 'new' template" do
+        post :create, {:widget => { "name" => "invalid value" }}, valid_session
         expect(response).to render_template("new")
       end
     end
@@ -78,21 +76,18 @@ describe WidgetsController do
 
   describe "PUT update" do
     it "updates the requested widget" do
-      allow(Widget).to receive(:find).with(widget.to_param).and_return(widget)
       expect(widget).to receive(:update).with({ "name" => "MyString" })
       put :update, {:id => widget.to_param, :widget => { "name" => "MyString" }}, valid_session
     end
 
     describe "with valid params" do
       it "assigns the requested widget as @widget" do
-        allow(Widget).to receive(:find).and_return(widget)
         allow(widget).to receive(:update).with({ "name" => "MyString" })
         put :update, {:id => widget.to_param, :widget => valid_attributes}, valid_session
         expect(assigns(:widget)).to eq(widget)
       end
 
       it "redirects to the widget" do
-        allow(Widget).to receive(:find).and_return(widget)
         allow(widget).to receive(:update).with({ "name" => "MyString" }).and_return(true)
         put :update, {:id => widget.to_param, :widget => valid_attributes}, valid_session
         expect(response).to redirect_to(widget)
@@ -100,25 +95,21 @@ describe WidgetsController do
     end
 
     describe "with invalid params" do
-      before do
-        allow(widget).to receive(:update).and_return(false)
-        allow(Widget).to receive(:find).and_return(widget)
-        put :update, {:id => widget.to_param, :widget => { "name" => "invalid value" }}, valid_session
-      end
+      before { allow(widget).to receive(:update).and_return(false) }
 
       it "assigns the widget as @widget" do
+        put :update, {:id => widget.to_param, :widget => { "name" => "invalid value" }}, valid_session
         expect(assigns(:widget)).to eq(widget)
       end
 
       it "re-renders the 'edit' template" do
+        put :update, {:id => widget.to_param, :widget => { "name" => "invalid value" }}, valid_session
         expect(response).to render_template("edit")
       end
     end
   end
 
   describe "DELETE destroy" do
-    before { allow(Widget).to receive(:find).and_return(widget) }
-
     it "destroys the requested widget" do
       expect(widget).to receive(:destroy)
       delete :destroy, {:id => widget.to_param}, valid_session
