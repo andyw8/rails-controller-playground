a4783ae76c01810eae92a21746e88227ab5eb34c
Andy Waite <github.aw@andywaite.com>
2014-04-21 11:49:19 +0200

    Extract duplicate code to before/let blocks

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe WidgetsController do
   let(:valid_attributes) { { "name" => "MyString" } }
   let(:valid_session) { {} }
+  let(:widget) { Widget.create! valid_attributes }
 
   describe "GET index" do
     it "assigns all widgets as @widgets" do
-      widget = Widget.create! valid_attributes
       get :index, {}, valid_session
       expect(assigns(:widgets)).to eq([widget])
     end
@@ -14,7 +14,6 @@ describe WidgetsController do
 
   describe "GET show" do
     it "assigns the requested widget as @widget" do
-      widget = Widget.create! valid_attributes
       get :show, {:id => widget.to_param}, valid_session
       expect(assigns(:widget)).to eq(widget)
     end
@@ -29,7 +28,6 @@ describe WidgetsController do
 
   describe "GET edit" do
     it "assigns the requested widget as @widget" do
-      widget = Widget.create! valid_attributes
       get :edit, {:id => widget.to_param}, valid_session
       expect(assigns(:widget)).to eq(widget)
     end
@@ -43,28 +41,31 @@ describe WidgetsController do
         }.to change(Widget, :count).by(1)
       end
 
-      it "assigns a newly created widget as @widget" do
-        post :create, {:widget => valid_attributes}, valid_session
-        expect(assigns(:widget)).to be_a(Widget)
-        expect(assigns(:widget)).to be_persisted
-      end
+      context "after creation" do
+        before { post :create, {:widget => valid_attributes}, valid_session }
 
-      it "redirects to the created widget" do
-        post :create, {:widget => valid_attributes}, valid_session
-        expect(response).to redirect_to(Widget.last)
+        it "assigns a newly created widget as @widget" do
+          expect(assigns(:widget)).to be_a(Widget)
+          expect(assigns(:widget)).to be_persisted
+        end
+
+        it "redirects to the created widget" do
+          expect(response).to redirect_to(Widget.last)
+        end
       end
     end
 
     describe "with invalid params" do
-      it "assigns a newly created but unsaved widget as @widget" do
+      before do
         allow_any_instance_of(Widget).to receive(:save).and_return(false)
         post :create, {:widget => { "name" => "invalid value" }}, valid_session
+      end
+
+      it "assigns a newly created but unsaved widget as @widget" do
         expect(assigns(:widget)).to be_a_new(Widget)
       end
 
       it "re-renders the 'new' template" do
-        allow_any_instance_of(Widget).to receive(:save).and_return(false)
-        post :create, {:widget => { "name" => "invalid value" }}, valid_session
         expect(response).to render_template("new")
       end
     end
@@ -73,36 +74,32 @@ describe WidgetsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested widget" do
-        widget = Widget.create! valid_attributes
         expect_any_instance_of(Widget).to receive(:update).with({ "name" => "MyString" })
         put :update, {:id => widget.to_param, :widget => { "name" => "MyString" }}, valid_session
       end
 
       it "assigns the requested widget as @widget" do
-        widget = Widget.create! valid_attributes
         put :update, {:id => widget.to_param, :widget => valid_attributes}, valid_session
         expect(assigns(:widget)).to eq(widget)
       end
 
       it "redirects to the widget" do
-        widget = Widget.create! valid_attributes
         put :update, {:id => widget.to_param, :widget => valid_attributes}, valid_session
         expect(response).to redirect_to(widget)
       end
     end
 
     describe "with invalid params" do
-      it "assigns the widget as @widget" do
-        widget = Widget.create! valid_attributes
+      before do
         allow_any_instance_of(Widget).to receive(:save).and_return(false)
         put :update, {:id => widget.to_param, :widget => { "name" => "invalid value" }}, valid_session
+      end
+
+      it "assigns the widget as @widget" do
         expect(assigns(:widget)).to eq(widget)
       end
 
       it "re-renders the 'edit' template" do
-        widget = Widget.create! valid_attributes
-        allow_any_instance_of(Widget).to receive(:save).and_return(false)
-        put :update, {:id => widget.to_param, :widget => { "name" => "invalid value" }}, valid_session
         expect(response).to render_template("edit")
       end
     end
@@ -110,14 +107,13 @@ describe WidgetsController do
 
   describe "DELETE destroy" do
     it "destroys the requested widget" do
-      widget = Widget.create! valid_attributes
+      widget # reference the 'let' block to trigger the creation
       expect {
         delete :destroy, {:id => widget.to_param}, valid_session
       }.to change(Widget, :count).by(-1)
     end
 
     it "redirects to the widgets list" do
-      widget = Widget.create! valid_attributes
       delete :destroy, {:id => widget.to_param}, valid_session
       expect(response).to redirect_to(widgets_url)
     end
